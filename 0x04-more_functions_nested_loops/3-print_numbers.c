4b3d33a376b6f8de6b93ffaaaf7391a3fd8c81ce
ojoreborn <ojoreborn@DESKTOP-DONLKOD.localdomain>
2023-02-23 17:15:31 +0100

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number from 0 to 9
+ * Return: numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int p;
+
+	for (p = 0 ; p <= 9 ; p++)
+	{
+		_putchar(p + '0');
+	}
+	_putchar('\n');
+}
