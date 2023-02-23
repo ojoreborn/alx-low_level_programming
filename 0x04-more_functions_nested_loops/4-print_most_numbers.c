eb5a1a808c1785b1e54f4d99ee1c077145d14e83
ojoreborn <ojoreborn@DESKTOP-DONLKOD.localdomain>
2023-02-23 22:14:56 +0100

    task numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,18 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int x;
+	int ch;
 
-	for (x = 0 ; x <= 9 ; x++)
+	for (ch = 48 ; ch <= 58 ; ch++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
-		_putchar('\n');
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
 	}
+	_putchar('\n');
 }
