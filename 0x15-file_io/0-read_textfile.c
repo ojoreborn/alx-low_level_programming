4422d1284e514b52d4a4f2917dfb74a03f2fe4d2
ojoreborn <northlander59@gmail.com>
2023-04-08 16:49:14 +0100

    texfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,28 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - Read and print a text file to STDOUT.
- *
- * @filename: Name of the file to read.
- * @max_bytes: Maximum number of bytes to read.
- *
- * Return: Number of bytes read and printed to STDOUT, or -1 on failure.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-ssize_t read_textfile(const char *filename, size_t max_bytes)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = NULL;
-	ssize_t fd, bytes_read, bytes_written;
-	ssize_t total_bytes_written = 0;
-
-	if (filename == NULL)
-	return (-1);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	return (-1);
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	buffer = malloc(max_bytes);
-	if (buffer == NULL)
-	return (-1);
-
-	bytes_read = read(fd, buffer, max_bytes);
-	if (bytes_read == -1)
-	goto cleanup;
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	goto cleanup;
-
-	total_bytes_written = bytes_written;
-
-cleanup:
-	free(buffer);
+	free(buf);
 	close(fd);
-	return (total_bytes_written);
+	return (w);
 }
